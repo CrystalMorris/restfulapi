0ebc58ee86e35cf7839534792c7f0737c61c1856
CrystalM <81319362+CrystalMorris@users.noreply.github.com>
2021-06-15 21:35:24 -0500

    first commit

---
+++ src/test/java/org/generation/restfulapi/RestfulapiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation.restfulapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
